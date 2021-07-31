11f816eeb94c5081adaee9b2da2906dbd600f98e
Cainã Adrian Muniz <adrianmuniz.ceo@outlook.com>
2021-07-31 11:48:30 -0300

    Exercitando Desvio Condicionais

--- src/desvios/Exercicio04.java
+++ src/desvios/Exercicio04.java
@@ -1,11 +1,29 @@
 package desvios;
 
+import java.util.Scanner;
+
 public class Exercicio04 {
 
 	public static void main(String[] args) {
 		
 		
 		//Um robô de combate só deve ativar sua arma principal quando o inimigo está menos de 70c  de distancia . Faça um programa onde o usuário informe a distância do inimigo e sejam exibidas as mensagens "ATIVADO" ou " DESATIVADO" dependendo do status da arma
+		
+		Scanner leitor = new Scanner (System.in);
+		
+		int distancia;
+		
+		System.out.println("Programa para verificar a distancia minima para ativação da arma principal!");
+		System.out.print("Informe a distancia do inimigo: ");
+		distancia = leitor.nextInt();
+		
+		// Usando os desvios condicionais vamos verificar se a distancia informada pelo usuário é valida para ativação da arma principal
+		
+		if (distancia < 70) {
+			System.out.println("Arma principal DESATIVADA");
+		} else {
+			System.out.println("Arma principal ATIVADA");
+		}
 		
 		
 	}
